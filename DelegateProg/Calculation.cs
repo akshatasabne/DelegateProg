6c025704683b6d969d36d69eae2e673f3b66c85a
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-12 17:02:07 +0530

    Add project files.

---
+++ DelegateProg/Calculation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProg
+{
+    public delegate int MyDelegate(int a, int b);
+    public class Calculation
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Sub(int p,int q)
+        {
+            return p - q;
+        }
+    }
+    public delegate void MyDelegate1(int a, string b, double c);
+    public class Test
+    {
+        public void AcceptDetails(int a, string b, double c)
+        {
+            Console.WriteLine($"{a}--{b}--{c}");
+        }
+    }
+}
---
+++ DelegateProg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c= new Calculation();
+            MyDelegate d1=new MyDelegate(c.Add);
+            Console.WriteLine(d1.Invoke(4,5));
+            MyDelegate d2= new MyDelegate(c.Sub);
+            Console.WriteLine(d2.Invoke(8,4));
+
+            Test t = new Test();
+            MyDelegate1 d3 = new MyDelegate1(t.AcceptDetails);
+            d3.Invoke(1,"ak",50);
+
+        }
+    }
+}
