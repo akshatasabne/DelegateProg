67889d1c4e54bda756a06a4270a6bfbbf14a5316
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-07-13 10:58:32 +0530

    updated

---
+++ DelegateProg/MulticastDelegatepro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProg
+{
+    public class MulticastDelegatepro
+    {
+        public delegate int MyDelegate(int a, int b);
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int p, int q)
+        {
+            return p - q;
+        }
+    }
+}
--- DelegateProg/Program.cs
+++ DelegateProg/Program.cs
@@ -10,15 +10,26 @@ namespace DelegateProg
     {
         static void Main(string[] args)
         {
-            Calculation c= new Calculation();
-            MyDelegate d1=new MyDelegate(c.Add);
-            Console.WriteLine(d1.Invoke(4,5));
-            MyDelegate d2= new MyDelegate(c.Sub);
-            Console.WriteLine(d2.Invoke(8,4));
+            //Calculation c= new Calculation();
+            //MyDelegate d1=new MyDelegate(c.Add);
+            //Console.WriteLine(d1.Invoke(4,5));
+            //MyDelegate d2= new MyDelegate(c.Sub);
+            //Console.WriteLine(d2.Invoke(8,4));
 
-            Test t = new Test();
-            MyDelegate1 d3 = new MyDelegate1(t.AcceptDetails);
-            d3.Invoke(1,"ak",50);
+            //Test t = new Test();
+            //MyDelegate1 d3 = new MyDelegate1(t.AcceptDetails);
+            //d3.Invoke(1,"ak",50);
+
+            MulticastDelegatepro multicast=new MulticastDelegatepro();
+            MyDelegate d = new MyDelegate(multicast.Add);
+            d+= new MyDelegate(multicast.Sub);
+
+            Delegate[] del= d.GetInvocationList();
+            foreach(Delegate item in del)
+            {
+                Console.WriteLine(item.Method);
+                Console.WriteLine(item.DynamicInvoke(20,30));
+            }
 
         }
     }
